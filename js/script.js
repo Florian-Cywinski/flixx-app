905d4aeb4efc678a3c7542a98eae02b6c8310794
Florian-Cywinski <florian.cywinski@protonmail.com>
2023-12-28 00:58:47 +0100

    Initial commit and theme

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// developers.themoviedb.org/3/getting-started/introduction
+// developers.themoviedb.org/3/getting-started/request-rate-limiting
+// developers.themoviedb.org/3/search
+// https://www.themoviedb.org/
+// We using a library called swiper swiperjs.com
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Flixx App
+
+Vanilla JS app that displays current movie and TV show info - from Brad Trversy's 'Modern JS From The Beginning' course.
+
+Try out the app :) - https://
+

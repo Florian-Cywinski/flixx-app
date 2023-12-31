ccb40334ee8459994f3e565ae0ef71fb174d1c99
Florian-Cywinski <florian.cywinski@protonmail.com>
2023-12-31 14:55:28 +0100

    movie-details-page

--- js/script.js
+++ js/script.js
@@ -101,6 +101,65 @@ async function displayPopularShows() { // https://developer.themoviedb.org/refer
 }
 
 
+// Display Movie Details
+async function displayMovieDetails() {
+    const movieId = window.location.search.split('=')[1]; // To get the query string (everything from the ?) - window.location.search => ?id=848326 - ...split('=') => Array [ "?id", "848326" ] - [1] is the ID we need
+
+    const movie = await fetchAPIData(`movie/${movieId}`);
+
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="details-top">
+    <div>
+    ${  // We use the ternary-operator - if there is an image for this movie : if there isn't an image for this movie
+        movie.poster_path
+            ? `<img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.title}"
+          />`
+            : `<img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.title}"
+          />`
+    }
+    </div>
+    <div>
+      <h2>${movie.title}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${movie.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Release Date: ${movie.release_date}</p>
+      <p>
+        ${movie.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${movie.genres.map(genre => `<li>${genre.name}</li>`).join('')}
+      </ul>
+      <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Movie Info</h2>
+    <ul>
+      <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+      <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+      <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+      <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">
+        ${movie.production_companies.map(company => `<span>${company.name}</span>`).join(', ')}
+    </div>
+  </div>`;
+
+  document.getElementById('movie-details').appendChild(div);
+}
+
+
 // To show / hide the spinner
 function showSpinner() {
     document.querySelector('.spinner').classList.add('show');
@@ -110,6 +169,10 @@ function hideSpinner() {
 };
 
 
+// To add commas to the budget value
+function addCommasToNumber(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');    // This is called regular expression - Brad copied it from the internet - to add a comma after every three ziros
+}
 
 
 // Page Router
@@ -119,6 +182,7 @@ function hideSpinner() {
 const global = {    // Object of the global state
     currentPage: window.location.pathname,  // To set the property of currentPage to window.location.pathname
 };
+console.log(global.currentPage);
 
 const homeDirectory = '/11-flix-app-project/flixx-app/';    // To set the home directory in a const
 
@@ -136,6 +200,7 @@ function init() {
             break;
         case `${homeDirectory}movie-details.html`:  // For the case /11-flix-app-project/flixx-app/movie-details.html (movie-details.html)
             console.log('Movie Details');
+            displayMovieDetails();
             break;
         case `${homeDirectory}tv-details.html`:  // For the case /11-flix-app-project/flixx-app/tv-details.html (tv-details.html)
             console.log('TV Details');
@@ -157,10 +222,12 @@ function highlightActiveLink() {
         if (link.getAttribute('href') === global.currentPage) {
         // /11-flix-app-project/flixx-app === /11-flix-app-project/flixx-app/
             link.classList.add('active');
+        // } else if (link.getAttribute('href') === global.currentPage.substr(0, global.currentPage.length-10)) {  // For the case global.currentPage = /11-flix-app-project/flixx-app/index.html
+        } else if (global.currentPage.includes('index.html') && link.getAttribute('href') === '/11-flix-app-project/flixx-app/') {  // For the case global.currentPage = /11-flix-app-project/flixx-app/index.html
+            link.classList.add('active');
         }
     });
 };
 
 
 document.addEventListener('DOMContentLoaded', init);    // DOMContentLoaded runs as soon as the DOM is parsed and loaded - it calls the function init()
-

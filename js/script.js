3c7c3d51e7ebb666450edaeacf090f2f4fea377c
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-01-01 12:43:44 +0100

    details-page-backdrop

--- js/script.js
+++ js/script.js
@@ -107,6 +107,9 @@ async function displayMovieDetails() {
 
     const movie = await fetchAPIData(`movie/${movieId}`);
 
+    // Overlay for background image
+    displayBackgroundImage('movie', movie.backdrop_path);  // 'movie' is the type
+
     const div = document.createElement('div');
     div.innerHTML = `
     <div class="details-top">
@@ -172,6 +175,30 @@ function hideSpinner() {
 // To add commas to the budget value
 function addCommasToNumber(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');    // This is called regular expression - Brad copied it from the internet - to add a comma after every three ziros
+}
+
+
+// Display Backdrop On Details Pages (movies and shows)
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1'; // To be sure it's behind the content
+  overlayDiv.style.opacity = '0.1'; // To be faded
+
+  // To check the type (movie or show)
+  if (type === 'movie') {
+    document.getElementById('movie-details').appendChild(overlayDiv);
+  } else {
+    document.getElementById('show-details').appendChild(overlayDiv);
+  }
 }
 
 
